1d5b8e6e6ec9bc91cc1dfdf01c7b3422c59870d2
Mark Miles <mark.miles@students.fhu.edu>
2024-02-04 18:31:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,32 @@
 #as a string
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
-def parse_month(month):
-    month_dict = {
-        'January': '01',
-        'February': '02',
-        'March': '03',
-        'April': '04',
-        'May': '05',
-        'June': '06',
-        'July': '07',
-        'August': '08',
-        'September': '09',
-        'October': '10',
-        'November': '11',
-        'December': '12'
-    }
+month_dict = {
+    'January': '01',
+    'February': '02',
+    'March': '03',
+    'April': '04',
+    'May': '05',
+    'June': '06',
+    'July': '07',
+    'August': '08',
+    'September': '09',
+    'October': '10',
+    'November': '11',
+    'December': '12'
+}
+
+#def parse_month(month):
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formatted as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    date = parse_date.replace(',', '').split()
+    date = user_string.replace(',', '').split()
+    date[0] = month_dict[f"{date[0]}"]
+    if int(date[1]) < 10:
+        date[1] = '0' + date[1]
+    print(date[0], date[1], date[2], sep='/JN')
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
